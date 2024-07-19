44097f63829f00067cee0dbc96c11e5970df4be8
Nitheesh T Ganesh <iamntg@gmail.com>
2024-07-18 19:17:10 -0600

    Initial commit

---
+++ scaffold-web/src/styles/theme.tsx
@@ -0,0 +1,6 @@
+// import "@bcgov/bc-sans/css/BC_Sans.css";
+import { createAppTheme } from 'epic.theme';
+
+// Any theme overrides should be passed into the createAppTheme.
+
+export const theme = createAppTheme({});
